a377b221d7be632c2488c8f306685f00474697a4
ricardo8santana <luisriccardo@proton.me>
2025-08-11 17:28:34 -0300

    iniciando servidor com ejs

--- app.js
+++ app.js
@@ -3,10 +3,28 @@ const express = require('express');
 //Express aplicativo - configurando o acesso as funções 
 const app = express();
 
+//Registrar a visualização da engenharia
+app.set('view engine, ejs');
+
 //Ouvindo as requisições na porta
 app.listen(3001);
 
 //Acessando uma rota 
 app.get('/', (req, res)=>{
-    res.send('<p>Págibna inicial</p>')
+    res.render('index');
+});
+
+//Nova rota 
+app.get('/sobre', (req, res)=>{
+    res.render('sobre');
+});
+
+//Redirecionamento de página
+app.get('/sobrenos', (req, res)=>{
+    res.redirect('/sobre');
+});
+
+//Erro 404
+app.use((req, res)=>{
+    res.status(404).render();
 });
